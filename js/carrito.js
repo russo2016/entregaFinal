d02669aab39ff8254e56f1e29240700c2e2a4403
JRUSSO <russojoaco@gmail.com>
2023-08-03 15:57:35 -0300

    first commit

---
+++ js/carrito.js
@@ -0,0 +1,98 @@
+const productosCarrito = JSON.parse(localStorage.getItem("productosCarrito"))
+
+const carritoVacio = document.getElementById("carrito-vacio")
+const carritoProductos = document.getElementById("productos-carrito")
+const accionesCarrito = document.getElementById("acciones-carrito")
+const carritoComprado = document.getElementById("carrito-comprado")
+let btnEliminar = document.querySelectorAll(".eliminar-producto-carrito")
+const btnVaciar = document.getElementById("vaciar-acciones-carrito")
+const total = document.getElementById("total")
+const btnComprar = document.getElementById("pagar-acciones-carrito")
+
+
+function CargarCarrito(){
+    if (productosCarrito.length > 0){
+        carritoVacio.classList.add("deshabilitado")
+        carritoProductos.classList.remove("deshabilitado")
+        accionesCarrito.classList.remove("deshabilitado")
+        carritoComprado.classList.add("deshabilitado")
+
+        carritoProductos.innerHTML = ""
+        productosCarrito.forEach(producto =>{
+            const div = document.createElement("div")
+            div.classList.add("producto-carrito")
+            div.innerHTML = `
+            <img class="imagen-producto-carrito" src="${producto.imagen}" alt="${producto.titulo}">
+            <div class="titulo-producto-carrito">
+                <small>Producto</small>
+                <h3>${producto.titulo}</h3>
+            </div>
+            <div class="cantidad-producto-carrito">
+                <small>Cantidad</small>
+                <p>${producto.cantidad}</p>
+            </div>
+            <div class="precio-producto-carrito">
+                <small><P></P>Precio</small>
+                <p>$${producto.precio}</p>
+            </div>
+            <div class="subtotal-producto-precio">
+                <small>Subtotal</small>
+                <p>$${producto.precio*producto.cantidad}</p>
+            </div>
+            <button class="eliminar-producto-carrito" id="${producto.id}"><i class="bi bi-x"></i></button>
+            `
+            carritoProductos.append(div)
+        })
+    }else{
+        carritoVacio.classList.remove("deshabilitado")
+        carritoProductos.classList.add("deshabilitado")
+        accionesCarrito.classList.add("deshabilitado")
+        carritoComprado.classList.add("deshabilitado")
+    }
+    actualizarBtnsEliminar()
+    actualizarTotal()
+}
+
+CargarCarrito()
+
+function actualizarBtnsEliminar(){
+    btnEliminar = document.querySelectorAll(".eliminar-producto-carrito")
+    btnEliminar.forEach(boton =>{
+        boton.addEventListener("click",eliminarDelCarrito)
+    })
+}
+
+function eliminarDelCarrito(e){
+    const idBtn = e.currentTarget.id
+    const lugar = productosCarrito.findIndex(producto => producto.id === idBtn)
+    productosCarrito.splice(lugar,1)
+    CargarCarrito()
+    localStorage.setItem("productosCarrito",JSON.stringify(productosCarrito))
+}
+
+btnVaciar.addEventListener("click",vaciarCarrito)
+
+function vaciarCarrito(){
+    productosCarrito.length = 0
+    localStorage.setItem("productosCarrito",JSON.stringify(productosCarrito))
+    CargarCarrito()
+}
+
+function actualizarTotal(){
+    totalCalculado = productosCarrito.reduce((x,producto) => x + (producto.precio * producto.cantidad),0)
+    total.innerText = `$${totalCalculado}`
+}
+
+
+btnComprar.addEventListener("click",comprarCarrito)
+
+function comprarCarrito(){
+    productosCarrito.length = 0
+    localStorage.setItem("productosCarrito",JSON.stringify(productosCarrito))
+
+
+    carritoVacio.classList.add("deshabilitado")
+    carritoProductos.classList.add("deshabilitado")
+    accionesCarrito.classList.add("deshabilitado")
+    carritoComprado.classList.remove("deshabilitado")
+}
---
+++ js/main.js
@@ -0,0 +1,144 @@
+const productos =[
+    {
+        id:"vape-01",
+        titulo:"Supreme sin nicotina (2000pff)",
+        imagen: "img/Nuevo-proyecto-1-5.jpg",
+        precio:8000,
+        categoria:"2500pff"
+    },
+    {
+        id:"vape-02",
+        titulo:"Supreme (2000pff)",
+        imagen: "img/Nuevo-proyecto-1-5.jpg",
+        precio:8000,
+        categoria:"2500pff"
+    },
+    {
+        id:"vape-03",
+        titulo:"Fume Extra (1500pff)",
+        imagen: "img/Nuevo-proyecto-1-5.jpg",
+        precio:6950,
+        categoria:"2500pff"
+    },
+    {
+        id:"vape-04",
+        titulo:"Fume Ultra (2500pff)",
+        imagen: "img/Nuevo-proyecto-1-5.jpg",
+        precio:7700,
+        categoria:"2500pff"
+    },
+    {
+        id:"vape-05",
+        titulo:"Fume Infinity (3500pff)",
+        imagen: "img/Nuevo-proyecto-1-5.jpg",
+        precio:9700,
+        categoria:"5000pff"
+    },
+    {
+        id:"vape-06",
+        titulo:"Supreme (5000pff)",
+        imagen: "img/Nuevo-proyecto-1-5.jpg",
+        precio:10500,
+        categoria:"5000pff"
+    },
+    {
+        id:"vape-07",
+        titulo:"Fume Unlimited (7000pff)",
+        imagen: "img/Nuevo-proyecto-1-5.jpg",
+        precio:12500,
+        categoria:"7000pff"
+    },
+    {
+        id:"vape-08",
+        titulo:"Supreme (7000pff)",
+        imagen: "img/Nuevo-proyecto-1-5.jpg",
+        precio:13000,
+        categoria:"7000pff"
+    }
+]
+
+const containerProductos = document.getElementById("container-productos");
+const btnCategorias = document.querySelectorAll(".btn-menu")
+const titulo = document.getElementById("titulo-principal")
+let btnsAgregar = document.querySelectorAll(".agregar-producto")
+const num = document.getElementById("num")
+
+
+function cargarProductos(productosElegidos){
+
+    containerProductos.innerHTML = ""
+    productosElegidos.forEach(producto =>{
+        let div = document.createElement("div")
+        div.classList.add("producto")
+        div.innerHTML = `
+            <img class="imagen-producto" src="${producto.imagen}" alt="${producto.titulo}">
+            <div class="detalles-producto">
+                <h3 class="titulo-producto">${producto.titulo}</h3>
+                <p class="precio-producto">$${producto.precio}</p>
+                <button class="agregar-producto" id="${producto.id}">comprar</button>
+            </div>
+        `
+
+        containerProductos.append(div)
+    })
+
+    actualizarBtnsAgregar()
+}
+
+cargarProductos(productos)
+
+btnCategorias.forEach(boton =>{
+    boton.addEventListener("click", (e) =>{
+        btnCategorias.forEach(boton =>boton.classList.remove("activo"))
+        e.currentTarget.classList.add("activo")
+
+        if (e.currentTarget.id != "todos"){
+            const categoriaProducto = productos.find(producto => producto.categoria === e.currentTarget.id)
+
+            titulo.innerText = "Vaporizadores de " + categoriaProducto.categoria
+            const btnProductos = productos.filter(producto => producto.categoria === e.currentTarget.id)
+            cargarProductos(btnProductos)
+        }else{
+            titulo.innerText = "Todos los productos"
+            cargarProductos(productos)
+        }
+    })
+})
+
+function actualizarBtnsAgregar(){
+    btnsAgregar = document.querySelectorAll(".agregar-producto")
+    btnsAgregar.forEach(boton =>{
+        boton.addEventListener("click",agregarCarrito)
+    })
+}
+
+let carrito
+const carritoLS = localStorage.getItem("productosCarrito")
+if(carritoLS){
+    carrito = JSON.parse(carritoLS)
+    actualizarNum()
+}else{
+    carrito = []
+}
+
+function agregarCarrito(e){
+    const idBtn = e.currentTarget.id
+    const productoAgregado = productos.find(producto => producto.id === idBtn)
+
+    if(carrito.some(producto => producto.id === idBtn)){
+        const lugar = carrito.findIndex(producto => producto.id === idBtn)
+        carrito[lugar].cantidad +=1
+    }else{
+        productoAgregado.cantidad = 1
+        carrito.push(productoAgregado)
+    }
+    actualizarNum()
+
+    localStorage.setItem("productosCarrito",JSON.stringify(carrito))
+}
+
+function actualizarNum(){
+    let numero = carrito.reduce((x,producto) => x + producto.cantidad, 0)
+    num.innerText = numero
+}
+
